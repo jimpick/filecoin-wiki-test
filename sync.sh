415026f8400b853e6f27186a1beec447d7a07ab6
Jim Pick <jim@backblend.com>
2020-08-11 16:38:57 -0700

    New results

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-#rsync -vanP --exclude '*.bin' --exclude '*.zip.??' calibration-0:~/tmp/wiki-large-blocks/* wiki-large-blocks/
-rsync -vaP --exclude '*.bin' --exclude '*.zip.??' --delete calibration-0:~/tmp/wiki-large-blocks/* wiki-large-blocks/
+#rsync -vanP --exclude '*.bin' --exclude '*.zip.??' calibration-0:~/tmp/wiki-large-blocks/* wiki-large-blocks
+rsync -vaP --exclude '*.bin' --exclude '*.zip.??' --delete --delete-excluded calibration-0:~/tmp/wiki-large-blocks .
