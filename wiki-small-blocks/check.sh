af623a433a05d03f364ef79478823db6feb66c7d
Jim Pick <jim@jimpick.com>
2020-08-24 06:14:12 -0700

    Safer temp dir usage

--- wiki-small-blocks/check.sh
+++ wiki-small-blocks/check.sh
@@ -11,13 +11,23 @@ if [ -z "$CLIENT" ]; then
   lotus client list-deals -v > $OUTPUT
 fi
 
+WORKDIR=$(mktemp -d -t blaster-deals.XXXXXXX)
+function cleanup {
+  if [ -d "$WORKDIR" ]; then
+    rm -rf "$WORKDIR"
+  fi
+}
+trap cleanup EXIT
+
+
 for x in *.zip.??.??; do
   echo $x 1>&2
   echo $x
-  cat $x*.deal > tmp/deals.txt
-  grep -f tmp/deals.txt $OUTPUT | sort -k2
+  cat $x*.deal > $WORKDIR/deals.txt
+  grep -f $WORKDIR/deals.txt $OUTPUT | sort -k2
   echo
 done
+
 
 #lotus client list-deals -v | grep -f ~/tmp/deals.txt  | sort -k2 | tee ~/tmp/check.out
 
