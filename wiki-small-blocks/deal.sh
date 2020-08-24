7c1d6b7c4695d0b4c514cf3cc8a81327e3ca2a83
Jim Pick <jim@jimpick.com>
2020-08-23 19:02:19 -0700

    Rename for multiple clients

--- wiki-small-blocks/deal.sh
+++ wiki-small-blocks/deal.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+CLIENT=$(lotus state lookup `lotus wallet default`)
+#echo Client: $CLIENT
+
 #set -x
 set -e
 
@@ -10,16 +13,19 @@ if [ -z "$MINER" ]; then
 	exit 1
 fi
 
-lotus wallet balance
+echo $CLIENT: $(lotus wallet balance)
 
 count=0
 total=`ls wiki*.cid | wc -l`
 for f in wiki*.cid; do
 	#echo $f $((++count)) of $total
-	DEAL_FILE=$(echo $f | sed 's,\.cid,,').deal
-	DEAL_FILE_MINER=$(echo $f | sed 's,\.cid,,').$MINER.deal
-	if [ -f "$DEAL_FILE" -o -f "$DEAL_FILE_MINER" ]; then
-		#echo "$DEAL_FILE" or "$DEAL_FILE_MINER" exists, skipping
+  BASE=$(echo $f | sed 's,\.cid,,')
+	DEAL_FILE=$BASE.$CLIENT.deal
+	DEAL_FILE_MINER=$BASE.$CLIENT.$MINER.deal
+  DEAL_FILES=$(ls $BASE.*.$MINER.deal $BASE.$MINER.deal 2> /dev/null || true)
+  #echo Jim $BASE $DEAL_FILE_MINER
+	if [ -f "$DEAL_FILE" -o -n "$DEAL_FILES" ]; then
+		#echo "$DEAL_FILE" or other deals exist, skipping
     echo -n .
     echo $((++count)) > /dev/null
 	else
@@ -29,7 +35,7 @@ for f in wiki*.cid; do
 		#lotus client deal `cat $f` $MINER 0.000000000000000006 600000 | tee -a $DEAL_FILE
 		lotus client deal `cat $f`  $MINER 0.000000000125000000 600000 | tee -a $DEAL_FILE
 		cp $DEAL_FILE $DEAL_FILE_MINER
-		lotus wallet balance
+    #echo $CLIENT: $(lotus wallet balance)
 		exit
 	fi
 done
