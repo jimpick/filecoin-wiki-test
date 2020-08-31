29e11cf75a861d932d7e340596d5502081360b78
Jim Pick <jim@jimpick.com>
2020-08-31 20:15:45 +0000

    Import missing files on secondary

--- wiki-small-blocks/import.sh
+++ wiki-small-blocks/import.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+CLIENT=$(lotus state lookup `lotus wallet default`)
+
 count=0
 total=`ls wiki*.zip.??.?? | wc -l`
 for f in wiki*.zip.??.??; do
 	echo $f $((++count)) of $total
-	lotus client import $f | tee $f.import
+	lotus client import $f | tee $f.$CLIENT.import
 	cat $f.import | sed 's,^.*Root ,,' > $f.cid
 done
