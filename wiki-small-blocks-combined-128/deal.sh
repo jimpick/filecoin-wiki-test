b256441dd0a5d7a5d427d6c63ac2afeacb2244df
Jim Pick <jim@jimpick.com>
2020-10-15 02:00:12 +0000

    ovh-update

--- wiki-small-blocks-combined-128/deal.sh
+++ wiki-small-blocks-combined-128/deal.sh
@@ -18,7 +18,7 @@ echo $CLIENT: $(lotus wallet balance) "($FREE free)"
 
 count=0
 total=`ls *.cid | wc -l`
-for CID_FILE in `ls *.cid`; do
+for CID_FILE in `ls *.cid | grep -v wiki.10`; do
 	#echo $f $((++count)) of $total
 	BASE=$(echo $CID_FILE | sed 's,\.cid,,')
 	DEAL_FILE=$BASE.$CLIENT.deal
