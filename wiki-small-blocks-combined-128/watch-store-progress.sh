46dc700404856be0d1990bc4819535f413b13823
Jim Pick <jim@jimpick.com>
2020-12-02 06:30:31 +0000

    ovh-update

--- wiki-small-blocks-combined-128/watch-store-progress.sh
+++ wiki-small-blocks-combined-128/watch-store-progress.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-watch 'grep candidate ~/workshop-client-testnet/src/annotations-spacerace-slingshot-medium.js | wc -l ; ./parse-git-deal-candidates.sh | wc -l; ls skip-miners/ | wc -l'
+watch 'grep candidate ~/workshop-client-testnet/src/annotations-spacerace-slingshot-medium.js | wc -l ; DONE=$(./parse-git-deal-candidates.sh | wc -l); echo $DONE; SKIP=$(ls skip-miners/ | wc -l); echo $SKIP; echo $((DONE + SKIP)); lotus wallet balance; lotus state market balance `lotus wallet default`
+'
