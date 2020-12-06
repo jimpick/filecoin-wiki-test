5abe2e6c555ccaef77124a2498406a577e5023ff
Jim Pick <jim@jimpick.com>
2020-12-06 02:21:21 +0000

    ovh-update

--- wiki-small-blocks-combined-128/store.sh
+++ wiki-small-blocks-combined-128/store.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+. ~/lotus/SETENV-LITE-9001
+./client.sh
+
 mkdir -p store-logs
 
 ./store-batch-worker.sh 2>&1 | tee -a store-logs/store-$(date -u +'%s').log
--- wiki-small-blocks-combined-128/update-ovh.sh
+++ wiki-small-blocks-combined-128/update-ovh.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+. ~/lotus/SETENV-LITE-9001
+
 ./update.sh && git add . && git commit -a -m 'ovh-update' && git push
 
